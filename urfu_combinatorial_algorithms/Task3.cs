5a46525aa1093c1801d32dce1b646df3a3045292
waverma <dddg.dddfgg@yandex.ru>
2021-06-21 20:47:15 +0500

    change some

--- urfu_combinatorial_algorithms/Task3.cs
+++ urfu_combinatorial_algorithms/Task3.cs
@@ -1,10 +1,7 @@
 ﻿using CombinatorialAlgorithms;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace urfu_combinatorial_algorithms
 {
@@ -44,12 +41,81 @@ namespace urfu_combinatorial_algorithms
             File.WriteAllLines(path, data);
         }
 
+        public (Info, Dictionary<int, int>) Sort(Info inputData)
+        {
+            // НАЧАЛО ТОПОЛОГИЧЕСКОЙ СОРТИРОВКИ
+            var newGraph = new List<List<(int, int)>>();
+            var savedGraph = new List<List<(int, int)>>();
+            for (int i = 0; i < inputData.Next.Count(); i++)
+            {
+                var l = new List<(int, int)>();
+                for (int j = 0; j < inputData.Next[i].Count; j++)
+                {
+                    l.Add(inputData.Next[i][j]);
+                }
+                savedGraph.Add(l);
+            }
+
+            var translation = new Dictionary<int, int>();
+            var maxUsefullNum = inputData.Next.Count() - 1;
+
+
+            
+
+            while (maxUsefullNum >= 0)
+            {
+                var cur = -1;
+                for (var i = 0; i < savedGraph.Count(); i++)
+                    if (savedGraph[i] != null && savedGraph[i].Count == 0)
+                    {
+                        translation[cur = i] = maxUsefullNum--;
+                        break;
+                    }
+
+                savedGraph[cur] = null;
+                for (var i = 0; i < savedGraph.Count; i++)
+                {
+                    if (savedGraph[i] is null) continue;
+                    savedGraph[i] = savedGraph[i].Where(x => x.Item1 != cur).ToList();
+                }
+            }
+
+
+
+            var result = new Info();
+            result.Start = translation[inputData.Start];
+            result.Target = translation[inputData.Target];
+            var a = new List<(int, int)>[translation.Count];
+            for (int i = 0; i < translation.Count; i++)
+            {
+                a[translation[i]] = inputData.Next[i];
+                for (int j = 0; j < a[translation[i]].Count; j++)
+                {
+                    a[translation[i]][j] = (translation[a[translation[i]][j].Item1], a[translation[i]][j].Item2);
+                }
+            }
+            result.Next = a.ToList();
+            // КОНЕЦ ТОПОЛОГИЧЕСКОЙ СОРТИРОВКИ
+
+            var reverseTranslation = new Dictionary<int, int>();
+            foreach (var key in translation)
+            {
+                reverseTranslation[key.Value] = key.Key;
+            }
+            return (result, reverseTranslation);
+        }
+
         public List<string> Solve(Info inputData)
         {
+            var (d, t) = Sort(inputData);
+            inputData = d;
             var D = new int[inputData.Next.Count];
             var previous = new int[D.Length];
+            var result1 = new List<string> { "N" };
 
-            for (var i = 1; i < D.Length; i++) D[i] = int.MaxValue;
+
+            for (var i = 0; i < D.Length; i++) D[i] = int.MaxValue;
+            D[inputData.Start] = 0;
 
             for (var i = 0; i < D.Length - 1; i++)
             {
@@ -67,17 +133,17 @@ namespace urfu_combinatorial_algorithms
                         var n = inputData.Target;
                         while (n != 0)
                         {
-                            result.Add(n.ToString());
+                            result.Add(t[n].ToString());
                             n = previous[n];
                         }
 
                         result.Reverse();
-                        return new List<string> { "Y", string.Join(" ", result), D[v].ToString() };
+                        result1 = new List<string> { "Y", string.Join(" ", result), D[v].ToString() };
                     }
                 }
             }
 
-            return new List<string> { "N" };
+            return result1;
         }
 
         public void Solve(string inputFilePath, string outputFilePath)
