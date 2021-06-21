839a88d840525669e07078e04934838f95d26f02
waverma <dddg.dddfgg@yandex.ru>
2021-06-21 20:51:05 +0500

    change some

--- urfu_combinatorial_algorithms/Program.cs
+++ urfu_combinatorial_algorithms/Program.cs
@@ -13,7 +13,6 @@ namespace CombinatorialAlgorithms
         
         public static void Main(string[] args)
         {
-            //if (args.Length == 1 && int.TryParse(args[0], out var result)) TASK_NUMBER = result;
             
             var tasksContainer = new List<Lazy<ITask>>
             {
@@ -21,21 +20,6 @@ namespace CombinatorialAlgorithms
                 new Lazy<ITask>(() => new Task2()),
                 new Lazy<ITask>(() => new Task3()),
             };
-
-            // if (!(args is null) && args.Length > 0)
-            // {
-            //     var parsedArg = string.Join(" ", args)
-            //         .Split('\'')
-            //         .Where(x => x != " ")
-            //         .Where(x => x != "")
-            //         .ToArray();
-            //     
-            //     if (parsedArg.Length != 3) throw new ArgumentException(string.Join(" ", args));
-            //
-            //     inputFileName = parsedArg[0];
-            //     outputFileName = parsedArg[1];
-            //     taskNumber = int.Parse(parsedArg[2]);
-            // }
 
             tasksContainer[TASK_NUMBER].Value.Solve(IN_FILE_NAME, OUT_FILE_NAME);
         }
